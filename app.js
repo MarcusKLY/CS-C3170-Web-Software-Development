790b5e0325a2fbf1752a0854e7be5ca69e0e6fc0
Marcus <lykam@connect.hku.hk>
2024-11-06 21:34:44 +0200

    .

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import { addSong, getSongs } from "./songService.js";
+import { addSong, getSongs } from "songService.js";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 const app = new Hono();
