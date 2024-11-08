791544ef466a86ff0a013bb1786b9ae02ecc0864
Marcus <lykam@connect.hku.hk>
2024-11-08 15:11:38 +0200

    CRUD	operation for todo app

--- todoController.js
+++ todoController.js
@@ -3,7 +3,11 @@ import * as todoService from "./todoService.js";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 
-const showForm = (c) => c.html(eta.render("todos.eta"));
+const showForm = async (c) => {
+  return c.html(
+    eta.render("todos.eta", { todos: await todoService.listTodos() }),
+  );
+};
 
 const createTodo = async (c) => {
   const body = await c.req.parseBody();
--- todoService.js
+++ todoService.js
@@ -5,4 +5,16 @@ const createTodo = async (todo) => {
   await kv.set(["todos", todo.id], todo);
 };
 
-export { createTodo };
+const listTodos = async () => {
+  const kv = await Deno.openKv();
+  const todoEntries = await kv.list({ prefix: ["todos"] });
+
+  const todos = [];
+  for await (const entry of todoEntries) {
+    todos.push(entry.value);
+  }
+
+  return todos;
+};
+
+export { createTodo, listTodos };
