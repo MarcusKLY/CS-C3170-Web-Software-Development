59a9b2c0b152eec2ace8e4e42004e2db3763936e
Marcus <lykam@connect.hku.hk>
2024-11-08 14:41:01 +0200

    CRUD

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import { addSong, getSongs } from "songService.js";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+
 const app = new Hono();
 
-app.get("/", async (c) => {
-  const songs = getSongs();
-  return c.html(await eta.render("index.eta", { songs }));
-});
-
-app.post("/songs", async (c) => {
-  const { name, duration } = await c.req.parseBody();
-  addSong(name, parseInt(duration, 10));
-  return c.redirect("/");
-});
+app.get("/todos", (c) => c.html(eta.render("todos.eta")));
 
 Deno.serve(app.fetch);
--- songService.js
+++
@@ -1,9 +0,0 @@
-const songs = [];
-
-export function addSong(name, duration) {
-    songs.push({ name, duration });
-}
-
-export function getSongs() {
-    return songs;
-}
