0d4fcaf59db4d5ed759708657a8e2b075c5bf486
Marcus <lykam@connect.hku.hk>
2024-11-14 15:36:46 +0200

    Feedback

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
-// import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-// import * as todoController from "./bookController.js";
-// import	* as songController from "./songController.js";
-
-// const app = new Hono();
-
-// app.get("/", songController.showForm);
-// app.post("/songs", songController.createSong);
-
-// app.get("/books", todoController.showForm);
-// app.get("/books/:id", todoController.showTodo);
-// app.post("/books", todoController.createTodo);
-// app.post("/books/:id", todoController.updateTodo);
-// app.post("/books/:id/delete", todoController.deleteTodo);
-
-// Deno.serve(app.fetch);
-
-// -----
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
 import * as feedbacks from "./feedbacks.js";
+import { renderFile } from "https://deno.land/x/eta/mod.ts";
 
 
 const app = new Hono();
