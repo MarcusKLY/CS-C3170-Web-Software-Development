4d6f60c490bdf62adbdaf7fe9e207844a6b09db1
Marcus <lykam@connect.hku.hk>
2024-11-14 15:48:57 +0200

    fb

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import * as feedbacks from "./feedbacks.js";
 
 // Set the views folder
-const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 
 const app = new Hono();
 
-app.get("/", async (c) => {
-  const html = await eta.renderFile("index.eta");
-  return c.html(html);
-});
+app.get("/", async (c) => {feedbacks.showForm(c)});
 
-app.get("/feedbacks/:id", async (c) => {
-  const id = c.req.param("id");
-  const feedbackCount = await feedbacks.getFeedbackCount(id);
-  return c.text(`Feedback ${id}: ${feedbackCount}`);
-});
+// app.get("/feedbacks/:id", async (c) => {
+//   const id = c.req.param("id");
+//   const feedbackCount = await feedbacks.getFeedbackCount(id);
+//   return c.text(`Feedback ${id}: ${feedbackCount}`);
+// });
 
-app.post("/feedbacks/:id", async (c) => {
-  const id = c.req.param("id");
-  await feedbacks.incrementFeedbackCount(id);
-  return c.text(`OK`);
-});
+// app.post("/feedbacks/:id", async (c) => {
+//   const id = c.req.param("id");
+//   await feedbacks.incrementFeedbackCount(id);
+//   return c.text(`OK`);
+// });
 
 Deno.serve(app.fetch);
