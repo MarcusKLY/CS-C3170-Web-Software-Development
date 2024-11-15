c835ef0b24db0da088f40d46b59627c8e688acd5
Marcus <lykam@connect.hku.hk>
2024-11-15 16:44:30 +0200

    Update Cookie

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import { getCookie, setCookie } from "https://deno.land/x/hono@v3.12.11/helper.ts";
+import {
+  getCookie,
+  setCookie,
+} from "https://deno.land/x/hono@v3.12.11/helper.ts";
 
 const app = new Hono();
 
