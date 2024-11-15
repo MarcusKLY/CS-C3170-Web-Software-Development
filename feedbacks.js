9257629177d2ba3cc81d36e18ffdd375ba8f078f
Marcus <lykam@connect.hku.hk>
2024-11-15 10:48:14 +0200

    Providing course feedback

--- feedbacks.js
+++ feedbacks.js
@@ -15,7 +15,8 @@ const incrementFeedbackCount = async (courseId, id) => {
     const count = await getFeedbackCount(courseId, id);
     await kv.set(["feedbacks", courseId, id], count + 1);
   } catch (error) {
-    console.error("Error incrementing feedback count:", error);
+    console.error(`Error incrementing feedback count for courseId: ${courseId}, id: ${id}`, error);
     throw new Error("Could not increment feedback count.");
   }
 };
+
