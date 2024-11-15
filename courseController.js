2ab53359f6e0409c1a45425c761cfa8d118de8cc
Marcus <lykam@connect.hku.hk>
2024-11-15 15:25:49 +0200

    Course Feedback

--- courseController.js
+++ courseController.js
@@ -1,7 +1,13 @@
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import * as courseService from "./courseService.js";
+import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
+
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+
+const courseSchema = z.object({
+  name: z.string().min(4, "The course name should be a string of at least 4 characters."),
+});
 
 const showForm = async (c) => {
   return c.html(
@@ -11,6 +17,27 @@ const showForm = async (c) => {
 
 const createCourse = async (c) => {
   const body = await c.req.parseBody();
+  const course = {
+    name: body.name,
+  };
+
+  const validationResult = courseSchema.safeParse(course);
+
+  if (!validationResult.success) {
+    const errors = {};
+    validationResult.error.errors.forEach((e) => {
+      const field = e.path[0];
+      if (!errors[field]) {
+        errors[field] = { _errors: [] };
+      }
+      errors[field]._errors.push(e.message);
+    });
+
+    return c.html(
+      eta.render("courses.eta", { courses: await courseService.listCourses(), errors }),
+    );
+  }
+
   await courseService.createCourse(body);
   return c.redirect("/courses");
 };
