5f020c9597efdedc13656b5609e32d9cd769680d
Marcus <lykam@connect.hku.hk>
2024-11-15 22:02:56 +0200

    paywall

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import { getCookie, setCookie } from "https://deno.land/x/hono@v3.12.11/helper.ts";
+import {   getSignedCookie,
+  setSignedCookie,getCookie, setCookie } from "https://deno.land/x/hono@v3.12.11/helper.ts";
 
 const sessionCounts = new Map();
 const app = new Hono();
