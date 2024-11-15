c8a264279353ad2c9a52c328cdbfcbac1dc2d826
Marcus <lykam@connect.hku.hk>
2024-11-15 11:20:13 +0200

    t

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = new Hono();
 
 app.get("/courses/:courseId/feedbacks/:feedbackId", async (c) => {
   try {
-    const feedbackId = c.req.param("id");
+    const feedbackId = c.req.param("feedbackId");
     const courseId = c.req.param("courseId");
 
     if (!courseId && !feedbackId) {
