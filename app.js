22a54f3ddc248b322c0a0ced163f8f37d9af5e19
Marcus <lykam@connect.hku.hk>
2024-11-15 22:35:00 +0200

    coursefeedback

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ import * as courseController from "./courseController.js";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 const app = new Hono();
+const sessionCounts = new Map();
+const secret = "secret";
+
+const getAndIncrementCount = (sessionId, courseId) => {
+  if (!sessionCounts.has(sessionId)) {
+    sessionCounts.set(sessionId, new Map());
+  }
+
+  const courseCounts = sessionCounts.get(sessionId);
+  let count = courseCounts.get(courseId) ?? 0;
+  count++;
+  courseCounts.set(courseId, count);
+
+  return count;
+};
 
 app.get("/courses/:courseId/feedbacks/:feedbackId", async (c) => {
   const courseId = c.req.param("courseId");
@@ -15,11 +30,24 @@ app.get("/courses/:courseId/feedbacks/:feedbackId", async (c) => {
 });
 
 app.post("/courses/:courseId/feedbacks/:feedbackId", async (c) => {
-  const courseId = c.req.param("courseId");
-  const feedbackId = c.req.param("feedbackId");
-  await feedbacks.incrementFeedbackCount(courseId, feedbackId);
-  return c.redirect(`/courses/${courseId}`);
+  try {
+			const sessionId = await getSignedCookie(c, secret, "sessionId") ??
+    	crypto.randomUUID();
+			await setSignedCookie(c, "sessionId", sessionId, secret, {
+			path: "/",
+  	});
+			const courseId = c.req.param("courseId");
+			const feedbackId = c.req.param("feedbackId");
+			const count = getAndIncrementCount(sessionId, courseId);
+			await feedbacks.incrementFeedbackCount(courseId, feedbackId);
+			return c.redirect(`/courses/${courseId}`, {count});
+  } catch (error) {
+    console.error("Error in GET /:", error);
+    return c.text(`Internal Server Error: ${error}`, 500);
+  }
 });
+
+
 
 app.get("/courses", courseController.showForm);
 app.get("/courses/:id", courseController.showCourse);
