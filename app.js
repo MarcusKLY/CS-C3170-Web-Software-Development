8543fe0243b098c078b9a97129dfeef9755aad7b
Marcus <lykam@connect.hku.hk>
2024-11-16 13:49:02 +0200

    Limiting given course feedback

--- app.js
+++ app.js
@@ -1,69 +1,20 @@
-import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import {   getSignedCookie,
-  setSignedCookie,getCookie, setCookie } from "https://deno.land/x/hono@v3.12.11/helper.ts";
-
-import * as feedbacks from "./feedbacks.js";
 import * as courseController from "./courseController.js";
 
-const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 const app = new Hono();
-const sessionCounts = new Map();
-const secret = "secret";
 
-const getAndIncrementCount = (sessionId, courseId) => {
-  if (!sessionCounts.has(sessionId)) {
-    sessionCounts.set(sessionId, new Map());
-  }
-
-  const courseCounts = sessionCounts.get(sessionId);
-  let count = courseCounts.get(courseId) ?? 0;
-  count++;
-  courseCounts.set(courseId, count);
-
-  return count;
-};
-
-const getCount = (sessionId, courseId) => {
-		if (!sessionCounts.has(sessionId)) {
-				return 0;
-		}
-
-		const courseCounts = sessionCounts.get(sessionId);
-		return courseCounts.get(courseId) ?? 0;
-}
-
-app.get("/courses/:courseId/feedbacks/:feedbackId", async (c) => {
-  const courseId = c.req.param("courseId");
-  const feedbackId = c.req.param("feedbackId");
-  const feedbackCount = await feedbacks.getFeedbackCount(courseId, feedbackId);
-  return c.text(`Feedback ${feedbackId}: ${feedbackCount}`);
-});
-
-app.post("/courses/:courseId/feedbacks/:feedbackId", async (c) => {
-  try {
-			const sessionId = await getSignedCookie(c, secret, "sessionId") ??
-    	crypto.randomUUID();
-			await setSignedCookie(c, "sessionId", sessionId, secret, {
-			path: "/",
-  	});
-			const courseId = c.req.param("courseId");
-			const feedbackId = c.req.param("feedbackId");
-			await feedbacks.incrementFeedbackCount(courseId, feedbackId);
-			const count = getAndIncrementCount(sessionId, courseId);
-			return c.redirect(`/courses/${courseId}`);
-  } catch (error) {
-    console.error("Error in GET /:", error);
-    return c.text(`Internal Server Error: ${error}`, 500);
-  }
-});
-
-
+app.get(
+  "/courses/:courseId/feedbacks/:feedbackId",
+  courseController.getFeedbackCount,
+);
+app.post(
+  "/courses/:courseId/feedbacks/:feedbackId",
+  courseController.giveFeedback,
+);
 
 app.get("/courses", courseController.showForm);
 app.get("/courses/:id", courseController.showCourse);
 app.post("/courses", courseController.createCourse);
 app.post("/courses/:id/delete", courseController.deleteCourse);
 
-export { getAndIncrementCount, getCount };
 export default app;
--- courseController.js
+++ courseController.js
@@ -1,66 +1,103 @@
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
-import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 import {
   getSignedCookie,
   setSignedCookie,
 } from "https://deno.land/x/hono@v3.12.11/helper.ts";
 import * as courseService from "./courseService.js";
-import { getCount } from "./app.js";
+import * as feedbacks from "./feedbacks.js";
 
-const sessionCounts = new Map();
+
+const userCourseFeedbacks = new Map();
 const secret = "secret";
-
-const nameError = "The course name should be a string of at least 4 characters.";
-const courseValidator = z.object({
-  name: z.string({ message: nameError }).min(4, { message: nameError }),
-});
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 
+const setAndGetSessionId = async (c) => {
+  const sessionId = await getSignedCookie(c, secret, "sessionId") ??
+    crypto.randomUUID();
+  await setSignedCookie(c, "sessionId", sessionId, secret, {
+    path: "/",
+  });
+
+  return sessionId;
+};
+
+const hasGivenFeedback = async (userId, courseId) => {
+  return userCourseFeedbacks.has(userId) &&
+    userCourseFeedbacks.get(userId).has(courseId);
+};
+
+const setHasGivenFeedback = async (userId, courseId) => {
+  if (!userCourseFeedbacks.has(userId)) {
+    userCourseFeedbacks.set(userId, new Set());
+  }
+
+  userCourseFeedbacks.get(userId).add(courseId);
+};
+
 const showForm = async (c) => {
+  await setAndGetSessionId(c);
   return c.html(
     eta.render("courses.eta", { courses: await courseService.listCourses() }),
   );
 };
 
 const createCourse = async (c) => {
-  const body = await c.req.parseBody();
-  const validationResult = courseValidator.safeParse(body);
-  if (!validationResult.success) {
-    return c.html(eta.render("courses.eta", {
-      ...body,
-      courses: await courseService.listCourses(),
-      errors: validationResult.error.format(),
-    }));
-  }
+  await setAndGetSessionId(c);
 
+  const body = await c.req.parseBody();
   await courseService.createCourse(body);
   return c.redirect("/courses");
 };
 
 const showCourse = async (c) => {
-  try {
-    const id = c.req.param("id");
-    const sessionId = await getSignedCookie(c, secret, "sessionId") ?? crypto.randomUUID();
-    const count = await getCount(sessionId, id);
-    const course = await courseService.getCourse(id);
+  const userId = await setAndGetSessionId(c);
 
-    const renderedHtml = await eta.render("course.eta", { course, count });
-
-    return c.html(renderedHtml);
-  } catch (error) {
-    console.error("Error in showCourse:", error);
-    return c.text(`Internal Server Error: ${error}`, 500);
-  }
+  const id = c.req.param("id");
+  return c.html(
+    eta.render("course.eta", {
+      course: await courseService.getCourse(id),
+      feedbackAlreadyGiven: await hasGivenFeedback(userId, id),
+    }),
+  );
 };
 
-
-
-
 const deleteCourse = async (c) => {
+  await setAndGetSessionId(c);
+
   const id = c.req.param("id");
   await courseService.deleteCourse(id);
   return c.redirect("/courses");
 };
 
-export { createCourse, deleteCourse, showCourse, showForm };
+const giveFeedback = async (c) => {
+  const userId = await setAndGetSessionId(c);
+  const courseId = c.req.param("courseId");
+
+  if (await hasGivenFeedback(userId, courseId)) {
+    // has already given feedback
+    return c.redirect(`/courses/${courseId}`);
+  }
+
+  await setHasGivenFeedback(userId, courseId);
+
+  const feedbackId = c.req.param("feedbackId");
+  await feedbacks.incrementFeedbackCount(courseId, feedbackId);
+  return c.redirect(`/courses/${courseId}`);
+};
+
+const getFeedbackCount = async (c) => {
+  const courseId = c.req.param("courseId");
+  const feedbackId = c.req.param("feedbackId");
+  const feedbackCount = await feedbacks.getFeedbackCount(courseId, feedbackId);
+  return c.text(`Feedback ${feedbackId}: ${feedbackCount}`);
+};
+
+export {
+  createCourse,
+  deleteCourse,
+  getFeedbackCount,
+  giveFeedback,
+  showCourse,
+  showForm,
+};
