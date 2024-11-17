f078e38aac14e09bd24a4a301d69c1658b3bb30e
Marcus <lykam@connect.hku.hk>
2024-11-17 13:28:03 +0200

    User-Specific Data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
 import * as authController from "./authController.js";
+import * as todoController from "./todoController.js";
 import * as mainController from "./mainController.js";
 
 const app = new Hono();
@@ -10,6 +11,12 @@ app.get("/auth/registration", authController.showRegistrationForm);
 app.post("/auth/registration", authController.registerUser);
 app.post("/auth/logout", authController.logoutUser);
 
+app.get("/todos", todoController.showForm);
+app.get("/todos/:id", todoController.showTodo);
+app.post("/todos", todoController.createTodo);
+app.post("/todos/:id", todoController.updateTodo);
+app.post("/todos/:id/delete", todoController.deleteTodo);
+
 app.get("/", mainController.showMain);
 
-export	default app;
+export default app;
